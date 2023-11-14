c89ffd45e1ae944cc8f49411fcc506dd08f09ce1
Filip <filipbozovic998@gmail.com>
2023-11-14 23:39:09 +0100

    Update Mason

--- init.lua
+++ init.lua
@@ -24,12 +24,12 @@ require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, f
 
 vim.cmd.colorscheme "catppuccin-frappe"
 
-vim.opt.shell = 'powershell.exe'
-vim.opt.shellcmdflag = '-NonInteractive -NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
-vim.opt.shellxquote = ''
-vim.opt.shellquote = ''
-vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s'
-vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s'
+--vim.opt.shell = 'powershell.exe'
+--vim.opt.shellcmdflag = '-NonInteractive -NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+--vim.opt.shellxquote = ''
+--vim.opt.shellquote = ''
+--vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s'
+--vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s'
 
 --vim.opt.shell = 'wsl.exe'
 --vim.opt.shellcmdflag = 'ubuntu.exe run '
@@ -38,11 +38,11 @@ vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s'
 
 --vim.api.nvim_create_user_command("Pterm", 'term powershell.exe', {})
 --vim.api.nvim_create_user_command("PT", 'term powershell.exe', {})
-vim.api.nvim_create_user_command("Uterm", 'term wsl.exe', {})
-vim.api.nvim_create_user_command("UT", 'term wsl.exe', {})
+--vim.api.nvim_create_user_command("Uterm", 'term wsl.exe', {})
+--vim.api.nvim_create_user_command("UT", 'term wsl.exe', {})
 vim.api.nvim_create_user_command("BDAll", '%bd! | e#', {})
 vim.api.nvim_create_user_command("NT", 'bd! % | term', {})
-vim.api.nvim_create_user_command("NUT", 'bd! % | Uterm', {})
+--vim.api.nvim_create_user_command("NUT", 'bd! % | Uterm', {})
 
 
 
@@ -55,26 +55,32 @@ capabilities.offsetEncoding = { "utf-16" }
 require("lspconfig").clangd.setup({ capabilities = capabilities })
 require("mason-null-ls").setup({
     ensure_installed = {
-'diagnostic-languageserver',
-'alex',
-'actionlint',
-'autopep10',
-'clangd',
-'commitlint',
-'debugpy',
-'fixjson',
-'gitleaks',
-'isort',
-'lua-language-server',
-'prettier',
-'pyflakes',
-'python-lsp-server',
-'taplo',
-'typos',
-'yaml-language-server',
-'yamlfix',
+  'lua-language-server',
+  'ltex-ls',
+  'latexindent',
+  'vale',
+  'tectonic',
+  'actionlint',
+  'alex',
+  'clangd',
+  'commitlint',
+  'debugpy',
+  'diagnostic-languageserver',
+  'docker-compose-language-service',
+  'dockerfile-language-server',
+  'fixjson',
+  'gitleaks',
+  'isort',
+  'prettier',
+  'pyflakes',
+  'python-lsp-server',
+  'taplo',
+  'typos',
+  'yaml-language-server',
+  'yamlfix',
     },
     automatic_installation = false,
     handlers = {},
 })
+
 
