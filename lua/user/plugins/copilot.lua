654e49526519ebe9e9e7415573eb3d807ba25d27
Filip Bozovic <filip.bozovic@non.se.com>
2024-03-08 20:24:29 +0100

    Copilot

---
+++ lua/user/plugins/copilot.lua
@@ -0,0 +1,26 @@
+
+return {
+  "zbirenbaum/copilot.lua",
+  lazy = false,
+  opts = {
+  panel = {
+    auto_refresh = false,
+    keymap = {
+      accept = "<CR>",
+      jump_prev = "[[",
+      jump_next = "]]",
+      refresh = "gr",
+      open = "<M-CR>",
+    },
+  },
+  suggestion = {
+    auto_trigger = true,
+    keymap = {
+      accept = "<M-l>",
+      prev = "<M-[>",
+      next = "<M-]>",
+      dismiss = "<C-]>",
+    },
+  },
+  }
+}
