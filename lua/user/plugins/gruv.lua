e9e34682f1fd35c44573063b24a01102c501fc47
smrznuti1 <filipbozovic998@gmail.com>
2024-03-11 21:49:37 +0100

    Gruv

--- lua/user/plugins/gruv.lua
+++ lua/user/plugins/gruv.lua
@@ -1,2 +1,31 @@
-return { "ellisonleao/gruvbox.nvim", priority = 1000, lazy = false, opts = {transparent_mode = false, bold = true, 
-contrast = "high", terminal_colors = true}}
+return {
+  "ellisonleao/gruvbox.nvim",
+  lazy = false,
+
+  config = function()
+    require("gruvbox").setup {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    }
+  end,
+}
