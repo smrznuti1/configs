f1101b717c0fa6193c99e39fcbf4deb61a43a36a
smrznuti1 <filipbozovic998@gmail.com>
2024-04-06 22:10:17 +0200

    Changed LSP bundle to ~

--- lua/lspconfigs/pwsh_es.lua
+++ lua/lspconfigs/pwsh_es.lua
@@ -1,9 +1,7 @@
 local lspconfig = require "lspconfig"
-local bundle_path = "C:/LSP-s/PowerShellEditorServices"
+local bundle_path = "~/Lsp/PowerShellEditorServices"
 lspconfig.powershell_es.setup {
-  bundle_path = bundle_path,
-  filetypes = { "ps1" },
-  init_options = {
-    enableProfileLoading = false,
-  },
+    bundle_path = bundle_path,
+    filetypes = {"ps1"},
+    init_options = {enableProfileLoading = false}
 }
