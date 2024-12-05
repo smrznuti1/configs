e562608a8a4f7089cb0c5cd0f3528368061267f8
Filip Bozovic <filip.bozovic@non.se.com>
2024-12-05 16:31:49 +0100

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,6 +11,7 @@ if ([string]::IsNullOrEmpty($env:NVIM))
 {
   exit;
 }
+$ESC = [char]27
 
 function prompt
 {
@@ -28,7 +29,6 @@ function prompt
 
   $gitbranch = (git symbolic-ref --short HEAD 2>$null) -replace '\s', ''
 
-  $ESC = [char]27
   $gitbranchView = if ($gitbranch)
   {
 
@@ -59,6 +59,23 @@ function pwc
 }
 
 
+$PSStyle.FileInfo.Directory = '{0}[33m' -f $ESC
+$PSStyle.FileInfo.Executable = '{0}[32m' -f $ESC
+$PSStyle.FileInfo.Extension['.ps1'] = '{0}[34m' -f $ESC
+$PSStyle.FileInfo.Extension['.psd1'] = '{0}[34m' -f $ESC
+$PSStyle.FileInfo.Extension['.psm1'] = '{0}[34m' -f $ESC
+$PSStyle.FileInfo.Extension['.ps1'] = '{0}[34m' -f $ESC
+$PSStyle.FileInfo.Extension.Keys
+
+
+$PSStyle.FileInfo.Extension['.xml'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.zip'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.tgz'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.gz'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.tar'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.nupkg'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.cab'] = '{0}[31m' -f $ESC
+$PSStyle.FileInfo.Extension['.7z'] = '{0}[31m' -f $ESC
 
 Set-PSReadlineOption -Colors @{
   Command = [System.ConsoleColor]::DarkGreen
@@ -77,6 +94,6 @@ Set-PSReadlineOption -Colors @{
 }
 Set-PSReadLineOption -EditMode "Vi"
 
-Import-Module Terminal-Icons
+#Import-Module Terminal-Icons
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 catlogin
